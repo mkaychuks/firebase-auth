68f34b2be3ebe079362e700cc2585e599a6fadca
mkaychuks <sachufusi@gmail.com>
2022-08-15 18:42:42 +0100

    wrote to a DB on fireStore

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,18 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-import { auth } from "../firebase";
+import React, { useState } from "react";
+import { TextInput, Button } from "react-native-paper";
+import { collection, addDoc } from "firebase/firestore";
+
+// local imports
+import { auth, db } from "../firebase";
 import { signOut } from "firebase/auth";
 
 const HomeScreen = ({ navigation }) => {
+  const [surname, setSurname] = useState("");
+  const [address, setAddress] = useState("");
+  const [owner, setOwner] = useState(auth.currentUser?.email);
+
+  // handle signing user out
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -12,12 +21,52 @@ const HomeScreen = ({ navigation }) => {
       .catch((err) => alert(err.message));
   };
 
+  // adding something to the database
+  const addToDB = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "details"), {
+        surname: { surname },
+        address: { address },
+        owner: { owner },
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Email: {auth.currentUser?.email}</Text>
       <TouchableOpacity style={styles.button} onPress={handleSignOut}>
         <Text style={styles.buttonText}>Sign out</Text>
       </TouchableOpacity>
+
+      <View style={{ width: "75%", marginTop: 10 }}>
+        <TextInput
+          placeholder="surname"
+          mode="outlined"
+          activeOutlineColor="white"
+          outlineColor="white"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+        <TextInput
+          placeholder="address"
+          mode="outlined"
+          activeOutlineColor="white"
+          outlineColor="white"
+          value={address}
+          onChangeText={(text) => setAddress(text)}
+        />
+        <Button
+          onPress={addToDB}
+          mode="outlined"
+          style={{ marginTop: 15 }}
+        >
+          Submit
+        </Button>
+      </View>
     </View>
   );
 };
